babc18c663f4f375a978d0f2f377b98ae18c18d1
Joko Mulyono <jokomulyno696@gmail.com>
2020-11-10 12:53:37 +0700

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -x
     git init
-    git remote add deploy "git@github.com:Joko25/joko25.github.io.git"
+    git remote add deploy "https://github.com/Joko25/joko25.github.io.git"
     git config user.name "Joko Mulyono"
     git config user.email "jokomulyno696@gmail.com"
     git add .
     git commit -m "Deploy"
     git push deploy deploy --force
-fi
