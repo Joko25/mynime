19720325cfc1741c18c5ed22f4efc616297ece8e
Joko Mulyono <jokomulyno696@gmail.com>
2020-11-10 12:53:04 +0700

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'deploy' ] ; then
     git init
-    git remote add deploy "ssh://deploy@$REMOTE_ADDRESS:/home/deploy/shallty/.git"
-    git config user.name "Travis CI"
-    git config user.email "travisCI@gmail.com"
+    git remote add deploy "git@github.com:Joko25/joko25.github.io.git"
+    git config user.name "Joko Mulyono"
+    git config user.email "jokomulyno696@gmail.com"
     git add .
     git commit -m "Deploy"
     git push deploy deploy --force
-else
-    echo "Not deploying, since this branch isn't deploy."
 fi
